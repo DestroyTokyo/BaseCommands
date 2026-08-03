37c4a1d1b0e44213780f72092cd44a0e7a0460e1
Nionim <delta.cion.fe@gmail.com>
2026-08-03 11:55:47 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    java
+       id("com.gradleup.shadow") version "9.6.1"
+   }
+
+   group = "delta.cion.tokyo.baseCommands"
+
+   repositories {
+       mavenCentral()
+       maven("https://tokyo.citory.net/")
+   }
+
+   java {
+       toolchain {
+           languageVersion = JavaLanguageVersion.of(25)
+       }
+   }
+
+   dependencies {
+       compileOnly("delta.cion.tokyo:tokyo:2.3.0-predemo")
+   }
+
+   tasks {
+       build {
+           dependsOn(shadowJar)
+       }
+
+	shadowJar {
+       	mergeServiceFiles()
+       	archiveClassifier.set("")
+       }
+
+       withType<JavaCompile> {
+    		options.encoding = "UTF-8"
+       }
+   }
---
+++ src/main/java/delta/cion/tokyo/baseCommands/BaseCommands.java
@@ -0,0 +1,11 @@
+package delta.cion.tokyo.baseCommands;
+
+import delta.cion.tokyo.api.plugin.Plugin;
+
+public class BaseCommands extends Plugin {
+
+    public BaseCommands(String id, String name, String version) {
+        super(id, name, version);
+    }
+
+}
